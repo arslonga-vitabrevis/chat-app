49cc8553818d217174e98b097f3ea98e63c6c9b8
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-08-15 14:56:48 +0900

    メッセージの表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:create, :new]
+  resources :rooms, only: [:create, :new] do
+    resources :messages, only: [:new, :create, :index]
+  end
 end
