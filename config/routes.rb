9430e18e4d2fe5a1e02ce52795ae198cbb40bfc6
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-08-15 15:45:38 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:create, :new] do
+  resources :rooms, only: [:create, :new, :destroy] do
     resources :messages, only: [:new, :create, :index]
   end
 end
